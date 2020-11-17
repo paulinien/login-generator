39c56045bc3fcd16ff971e66bd2f56b096303740
paulinien <paulinien@live.fr>
2020-11-17 11:32:49 +0100

    Creation test CT1 #2
    Fix #3
    
    100% couverture des tests LoginGeneratorTest.java #2

---
+++ src/test/java/geco/LoginGeneratorTest.java
@@ -0,0 +1,50 @@
+package geco;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LoginGeneratorTest {
+
+    private LoginGenerator loginGenerator;
+    private LoginService loginService;
+    private String[] loginsString = {"JROL", "BPER", "CGUR", "JDU", "JRAL", "JRAL1"};
+
+    @Before
+    public void setUp() throws Exception {
+        loginService = new LoginService(loginsString);
+        loginGenerator = new LoginGenerator(loginService);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        loginService = null;
+        loginGenerator = null;
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom1() {
+        //CT1
+        assertEquals("PDUR", loginGenerator.generateLoginForNomAndPrenom("Durand", "Paul"));
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom2() {
+        //CT2
+        assertEquals("JRAL2", loginGenerator.generateLoginForNomAndPrenom("Ralling", "John"));
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom3() {
+        //CT3
+        assertEquals("JROL1", loginGenerator.generateLoginForNomAndPrenom("Rolling", "John"));
+    }
+
+    @Test
+    public void generateLoginForNomAndPrenom4() {
+        //CT4
+        assertEquals("PDUR", loginGenerator.generateLoginForNomAndPrenom("Dùrand", "Paul"));
+    }
+}
